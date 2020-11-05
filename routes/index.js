0e44d17a9e59b22dc8e0b83fa62f8849f8638b90
robherron <rlherron100@gmail.com>
2020-11-05 15:26:22 -0500

    Style changes

--- routes/index.js
+++ routes/index.js
@@ -14,7 +14,7 @@ router.get('/projects/:id', (req, res, next) => {
 const projectId = req.params.id;
 const getProjects = projects.find( ({id}) => id === +projectId);
 if (getProjects) {
-    res.render("project", { projects });
+    res.render("project", { projects: projects[req.params.id] });
 } 
 });
 
