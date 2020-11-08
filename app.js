100f00bb23fbc8bf84f58692becc0c5e621e5158
robherron <rlherron100@gmail.com>
2020-11-07 22:42:48 -0500

    temporarily removed favicon

--- app.js
+++ app.js
@@ -3,8 +3,9 @@ const sslRedirect = require('heroku-ssl-redirect').default;
 const express = require('express');
 const app = express();
 app.use(sslRedirect());
-favicon = require('serve-favicon');
-app.use(favicon( (__dirname + '/public/images/RH.png'));
+// favicon = require('serve-favicon');
+// app.use(favicon(path.join(__dirname,'public','images','favicon.ico')));
+
 const port = process.env.PORT || 3000;
 app.use('/static', express.static('public'));
 app.set('view engine', 'pug');
