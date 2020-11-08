77726ddef52144efa1c7a175cf1e8f644b07ea06
robherron <rlherron100@gmail.com>
2020-11-07 21:12:42 -0500

    fixes

--- app.js
+++ app.js
@@ -8,13 +8,21 @@ app.set('view engine', 'pug');
 const routes = require('./routes');
 app.use(routes);
 
-app.use((req, res, next) => {
-if (req.header('x-forwarded-proto') !== 'https')
-      res.redirect(`https://roblherron.com`);
-      else (next);
-});
+// app.use((req, res, next) => {
+// if (req.header('x-forwarded-proto') !== 'https')
+//       res.redirect(`https://roblherron.com`);
+//       else (next);
+// });
       
-  
+app.get('*', function(req, res, next) {
+    //http://docs.aws.amazon.com/ElasticLoadBalancing/latest/DeveloperGuide/TerminologyandKeyConcepts.html#x-forwarded-proto
+        if (req.get('x-forwarded-proto') != "https") {
+            res.set('x-forwarded-proto', 'https');
+            res.redirect('https://' + req.get('host') + req.url);
+        } else {
+            next();     
+        }
+    });
 
 //error handlers
 
